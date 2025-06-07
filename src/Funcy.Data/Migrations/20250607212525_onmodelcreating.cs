1e12c293c2331fa9eb4af960beb567382db693b4
Robin Rudenwall <r.rudenwall@gmail.com>
2025-06-07 23:36:45 +0200

    new migration

---
+++ src/Funcy.Data/Migrations/20250607212525_onmodelcreating.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Funcy.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class onmodelcreating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FunctionAppSlots_FunctionApps_FunctionAppId",
+                table: "FunctionAppSlots");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Functions_FunctionApps_FunctionAppId",
+                table: "Functions");
+
+            migrationBuilder.AddColumn<long>(
+                name: "FunctionAppId1",
+                table: "Functions",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "FunctionAppId1",
+                table: "FunctionAppSlots",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Functions_FunctionAppId1",
+                table: "Functions",
+                column: "FunctionAppId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FunctionAppSlots_FunctionAppId1",
+                table: "FunctionAppSlots",
+                column: "FunctionAppId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FunctionAppSlots_FunctionApps_FunctionAppId",
+                table: "FunctionAppSlots",
+                column: "FunctionAppId",
+                principalTable: "FunctionApps",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FunctionAppSlots_FunctionApps_FunctionAppId1",
+                table: "FunctionAppSlots",
+                column: "FunctionAppId1",
+                principalTable: "FunctionApps",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Functions_FunctionApps_FunctionAppId",
+                table: "Functions",
+                column: "FunctionAppId",
+                principalTable: "FunctionApps",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Functions_FunctionApps_FunctionAppId1",
+                table: "Functions",
+                column: "FunctionAppId1",
+                principalTable: "FunctionApps",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FunctionAppSlots_FunctionApps_FunctionAppId",
+                table: "FunctionAppSlots");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_FunctionAppSlots_FunctionApps_FunctionAppId1",
+                table: "FunctionAppSlots");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Functions_FunctionApps_FunctionAppId",
+                table: "Functions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Functions_FunctionApps_FunctionAppId1",
+                table: "Functions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Functions_FunctionAppId1",
+                table: "Functions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FunctionAppSlots_FunctionAppId1",
+                table: "FunctionAppSlots");
+
+            migrationBuilder.DropColumn(
+                name: "FunctionAppId1",
+                table: "Functions");
+
+            migrationBuilder.DropColumn(
+                name: "FunctionAppId1",
+                table: "FunctionAppSlots");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FunctionAppSlots_FunctionApps_FunctionAppId",
+                table: "FunctionAppSlots",
+                column: "FunctionAppId",
+                principalTable: "FunctionApps",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Functions_FunctionApps_FunctionAppId",
+                table: "Functions",
+                column: "FunctionAppId",
+                principalTable: "FunctionApps",
+                principalColumn: "Id");
+        }
+    }
+}
