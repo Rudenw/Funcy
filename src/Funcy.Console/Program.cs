15ed6a6355c3fdaa6feaffb0f86995aab322a4bf
Rudenw <r.rudenwall@gmail.com>
2026-03-04 23:18:01 +0100

    Load user settings from data directory

--- src/Funcy.Console/Program.cs
+++ src/Funcy.Console/Program.cs
@@ -30,6 +30,7 @@ var config = new ConfigurationBuilder()
     .SetBasePath(System.AppContext.BaseDirectory)
     .AddJsonFile("appsettings.json")
     .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Production"}.json", optional: true)
+    .AddJsonFile(Path.Combine(dataDirectory, "settings.json"), optional: true, reloadOnChange: false)
     .AddInMemoryCollection(new Dictionary<string, string?>
     {
         ["Serilog:WriteTo:0:Args:path"] = Path.Combine(dataDirectory, "logs", "funcy.log")
